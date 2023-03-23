2148dafa02fcc916da0022e63f1898aa1d04a3ce
rugendo <ercrugendo@gmail.com>
2023-03-23 19:08:16 +0300

    modified mongodb username

--- _config.js
+++ _config.js
@@ -2,9 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-
-    production: 'mongodb+srv://<Ericuser1>:<Q6GG2ooJV9kdiSrc>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<Ericuser1>:<Q6GG2ooJV9kdiSrc>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<Ericuser1>:<Q6GG2ooJV9kdiSrc>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Ericuser1:un511FOcbFn6E5CG@clusterip6.3bdres3.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Ericuser1:un511FOcbFn6E5CG@clusterip6.3bdres3.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Ericuser1:un511FOcbFn6E5CG@clusterip6.3bdres3.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
