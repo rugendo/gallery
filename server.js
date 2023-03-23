4a4d86de8c904f185f1058f0ad51012bd7d35d3c
rugendo <ercrugendo@gmail.com>
2023-03-23 16:45:04 +0300

    merged master and test branch

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-<<<<<<< HEAD
-let mongodb_url = 'mongodb://0.0.0.0:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-=======
 
 const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
@@ -26,7 +20,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
     }else{
         console.log(`Connected to Database: ${MONGODB_URI}`)
     }
->>>>>>> test
 });
 
 // test if the database has connected successfully
@@ -58,9 +51,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
-<<<<<<< HEAD
-=======
-
-
 module.exports = app;
->>>>>>> test
+
